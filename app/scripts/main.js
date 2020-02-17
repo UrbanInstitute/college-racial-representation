d060c3611ec9d6f13f778827e13f15253d93f462
Carman <jstraley@gmail.com>
2020-02-17 13:24:34 -0500

    adds colors named in SCSS and JavaScript to pretty close to basic project

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,3 +9,33 @@
 // $(function () { $('[data-toggle="popover"]').popover(); });
 
 
+	var margin = {top: 20, right: 20, bottom: 30, left: 50},
+	    width = 600 - margin.left - margin.right,
+	    height = 500 - margin.top - margin.bottom;
+
+	// parse the date / time
+
+
+	// append the svg obgect to the body of the page
+	// appends a 'group' element to 'svg'
+	// moves the 'group' element to the top left margin
+	var svg = d3.select("#exploreSciBar").append("svg")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+	  .append("g")
+	    .attr("transform",
+	          "translate(" + margin.left + "," + margin.top + ")");
+
+	// Get the data
+
+	Promise.all([
+	    d3.csv("file1.csv"),
+	    d3.csv("file2.csv"),
+	]).then(function(files) {
+	    // files[0] will contain file1.csv
+	    // files[1] will contain file2.csv
+	}).catch(function(err) {
+	    // handle error here
+	})
+
+	  // format the data
